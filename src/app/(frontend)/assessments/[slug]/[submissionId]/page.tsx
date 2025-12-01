3aa70e99b56f9d667d59b1d5db6dc62397f08c78
grafiswhello <gn.mailwork@gmail.com>
2025-12-02 00:35:22 +0800

    add: dummy assessment submission page

---
+++ src/app/(frontend)/assessments/[slug]/[submissionId]/page.tsx
@@ -0,0 +1,12 @@
+import Footer from '$layouts/Footer'
+import Header from '$layouts/Header'
+
+export default async function assessmentSubmissionPage() {
+	return (
+		<>
+			<Header />
+			<h1>Assessment Submission Page</h1>
+			<Footer />
+		</>
+	)
+}
